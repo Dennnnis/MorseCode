9d97a9061ed11a801ca06c9ee4b4e1c103919b3a
Dennnnis <dennisbeckertim@gmail.com>
2018-09-03 11:33:14 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MorseCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(555, 468);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(573, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(558, 468);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(566, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Morse";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 580);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MorseCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MorseCode
+{
+    public partial class Form1 : Form
+    {
+        public class Pair
+        {
+            public string morse;
+            public string text;
+        }
+        List<Pair> morse = new List<Pair>();
+
+        public Form1()
+        {
+            string[] lines = File.ReadAllLines("Translation.txt");
+            foreach (string line in lines)
+            {
+                morse.Add(new Pair() { morse = line.Split(' ')[0], text = line.Split(' ')[1]}) ;
+            }
+
+            InitializeComponent();
+        }
+
+        //Text input/output
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!textBox1.Focused) { return; }
+            string final = "";
+            string buffer = "";
+            foreach (char c in textBox1.Text)
+            {
+                if (c == ' ') { final += "/ ";  buffer = ""; continue; };
+                buffer += c.ToString().ToUpper();
+
+                foreach (Pair p in morse)
+                {
+                    if (p.text == buffer)
+                    {
+                        final += $"{p.morse} ";
+                        buffer = "";
+                        break;
+                    }                
+                }
+            }
+            textBox2.Text = final;
+        }
+
+        //Morse code input/output
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!textBox2.Focused) { return; }
+            string debug = "";
+            string final = "";
+            string buffer = "";
+            foreach (char c in textBox2.Text + " ")
+            {
+                debug += c;
+                if (c == '/') { final += ' ';  }
+                if (c == ' ')
+                {
+                    foreach (Pair p in morse)
+                    {
+                        if (p.morse == buffer)
+                        {
+                            debug += p.text;
+                            final += p.text;
+                        }
+                    }
+                    buffer = "";
+                    continue;
+                };
+                buffer += c.ToString().ToUpper();
+            }
+            textBox1.Text = final;
+        }
+    }
+}
